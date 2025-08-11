ee523ed1d36d0a8079473d98384c1b9319c7d00f
Gustavo Mendes <gstv.mms@gmail.com>
2025-08-11 16:59:15 -0300

    Secret Friend 1.0

---
+++ README.md
@@ -0,0 +1,38 @@
+# ONE: Oracle Next Education
+
+## Secret Friend Challenge
+
+Website that runs a Secret Friend game. Made with HTML, CSS and JavaScript for studies and portfolio purposes only. This assignment is part of the ONE: Oracle Next Education program.
+
+### Features
+
+- Can add or remove as many names as you want;
+- Randomly picks a name to be your Secret Friend;
+- Buttons are disabled as long as they can't do their job;
+- Can hide the name of the last name drawn in order to not spoil the secret when playing with more than one person;
+- Restart button (clears every entry and data to start a fresh game);
+- Fully responsive!
+
+### How does it work?
+
+1. Stores user inputs (secret friends' names) in an array, which is updated to a list and shown in screen as it grows or shrinks;
+2. When the `Draw a secret friend` button is pressed, an element from said array is picked randomly and is shown as your Secret Friend;
+3. The user can then continue playing, hide the last sorted name or restart the game.
+
+## Installation
+
+1. Download the `app.js`, `styles.css` files and the `assets` folder;
+2. Create a folder and paste all the files in it;
+3. Run the code on a Live Server or in your preferred way to do so.
+
+### Usage
+
+1. Type the name of your friends and click `Add` or press <kbd>Enter</kbd>;
+2. Names will be displayed as buttons, click on a name to remove it from the list;
+3. Click on `Draw a secret friend` to randomly pick a name;
+4. Click on `Hide secret name` to hide the result;
+5. Click on `Restart game` to clear everything and start a fresh game.
+
+### Screenshot
+
+![alt text](assets/screenshot.png)
---
+++ app.js
@@ -0,0 +1,93 @@
+let friendsArray = [];
+let drawBtn = document.querySelector(".button-draw");
+let nameInput = document.querySelector(".input-name");
+let addBtn = document.querySelector(".button-add");
+let errorMessage = document.querySelector(".errorMessage");
+let friendsList = document.querySelector("#friendsList");
+let nameBtn = document.querySelector(".button-name");
+let hideBtn = document.querySelector(".button-hide");
+let resetBtn = document.querySelector(".button-reset");
+let resultList = document.querySelector("#result");
+let resultMessage = document.querySelector("#secretFriend");
+let hiddenIcon = document.querySelector("#hiddenIcon");
+
+/* Updates and shows the current list of names */
+function screenList() {
+  friendsList.innerHTML = "";
+  friendsArray.forEach(friend => {
+    friendsList.innerHTML += `<li><button active class="nameButton" onclick="eraseFriend('${friend}')">${friend}<i class="fa-solid fa-circle-xmark"></i></button></li>`;
+  });
+  if (friendsArray == "") {
+    resetBtn.disabled = true;
+    drawBtn.disabled = true;
+  } else {
+    resetBtn.disabled = false;
+  }
+}
+
+/* Allows to confirm the input pressing Enter */
+nameInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    addBtn.classList.add("keyboard");
+    addFriend();
+    setTimeout(() => {
+      addBtn.classList.remove("keyboard");
+    }, 150);
+  }
+});
+
+/* Pushes the input to an array */
+function addFriend() {
+  let secretName = nameInput.value.trim();
+
+  if (secretName !== "") {
+    friendsArray.push(secretName);
+    errorMessage.hidden = true;
+    nameInput.value = "";
+    drawBtn.disabled = false;
+    screenList();
+  } else {
+    errorMessage.hidden = false;
+  }
+}
+
+/* Erases a name when pressing it's button */
+function eraseFriend(nameToRemove) {
+  friendsArray = friendsArray.filter(friend => friend !== nameToRemove);
+  screenList();
+}
+
+/* Picks a random name to be the Secret Friend */
+function drawFriend() {
+  let sortedFriend = friendsArray[parseInt(Math.random() * friendsArray.length + 1) - 1];
+  resultMessage.innerHTML = `<h3>${sortedFriend}!</h3>`;
+  resultMessage.hidden = false;
+  hideBtn.disabled = false;
+  hiddenIcon.hidden = true;
+}
+
+/* Hides or shows sorted name */
+function hideName() {
+  if (hiddenIcon.hidden == false && resultMessage.hidden == true) {
+    hiddenIcon.hidden = true;
+    resultMessage.hidden = false;
+    hideBtn.innerHTML = `<i class="fa-solid fa-eye-slash"></i>Hide secret name`;
+  } else {
+    resultMessage.hidden = true;
+    hiddenIcon.hidden = false;
+    hideBtn.innerHTML = `<i class="fa-solid fa-eye"></i>Show secret name`;
+  }
+  hideBtn.active = true;
+}
+
+/* Wipes everything and start a fresh new game */
+function newGame() {
+  friendsArray = [];
+  drawBtn.disabled = true;
+  hideBtn.disabled = true;
+  resetBtn.disabled = true;
+  hiddenIcon.hidden = true;
+  resultMessage.hidden = true;
+  resultMessage.innerHTML = "";
+  screenList();
+}
